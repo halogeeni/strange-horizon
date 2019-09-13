6ce4915b4102a7528548a2f9e24ed2c8b6d43b23
Aleksi Rasio <aleksi.rasio@gmail.com>
2019-09-14 00:03:09 +0300

    Implement HealthCheckController

--- src/main/java/com/arasio/webdev/strangehorizon/controllers/HealthCheckController.java
+++ src/main/java/com/arasio/webdev/strangehorizon/controllers/HealthCheckController.java
@@ -1,15 +1,21 @@
 package com.arasio.webdev.strangehorizon.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path="/healthcheck")
 public class HealthCheckController {
 
-    @RequestMapping("/healthcheck")
+    @Autowired
+    private BuildProperties buildProperties;
+
+    @RequestMapping("")
     @ResponseBody
-    public String healthcheck() {
-        return "I'm OK!";
+    public String checkSanity() {
+        return buildProperties.getName() + " " + buildProperties.getVersion();
     }
 }
--- src/test/java/com/arasio/webdev/strangehorizon/controllers/HealthCheckControllerTest.java
+++ src/test/java/com/arasio/webdev/strangehorizon/controllers/HealthCheckControllerTest.java
@@ -1,22 +1,27 @@
 package com.arasio.webdev.strangehorizon.controllers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class HealthCheckControllerTest {
+@AutoConfigureMockMvc
+class HealthCheckControllerTest {
 
     @Autowired
-    HealthCheckController controller;
+    private MockMvc mockMvc;
 
     @Test
-    public void returnsOkMessage() {
-        assertEquals("I'm OK!", controller.healthcheck());
+    void returnsOkStatusCode() throws Exception {
+        mockMvc.perform(get("/healthcheck"))
+            .andExpect(status().isOk());
     }
 }
