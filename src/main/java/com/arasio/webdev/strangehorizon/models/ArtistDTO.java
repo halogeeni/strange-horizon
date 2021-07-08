7e0c0aaafe1dd50dcd927af551a8d05318503afa
Aleksi Rasio <aleksi.rasio@gmail.com>
2021-07-08 10:17:34 +0300

    Add id field & new constructor to ArtistDTO

--- src/main/java/com/arasio/webdev/strangehorizon/models/ArtistDTO.java
+++ src/main/java/com/arasio/webdev/strangehorizon/models/ArtistDTO.java
@@ -8,14 +8,32 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class ArtistDTO {
 
     @JsonProperty
+    private int id;
+
+    @JsonProperty
     private String name;
 
     @JsonProperty
     private String description;
 
-    public ArtistDTO(String name, String description) {
+    public ArtistDTO(int id, String name, String description) {
+        this.id = id;
         this.name = name;
         this.description = description;
+    }
+
+    public ArtistDTO(ArtistEntity artistEntity) {
+        this.id = artistEntity.getId();
+        this.name = artistEntity.getName();
+        this.description = artistEntity.getDescription();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
